28b8f885448804679badd322e6cdf9a2a7168bc7
Jonathan Hironaga <jhiro009@gmail.com>
2020-06-03 15:42:41 -0400

    Remove failing test

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -4,8 +4,4 @@ describe('greet', () => {
   it('says hello with enthusiasm', () => {
     expect(greet('World')).toBe('Hello, World!');
   });
-
-  it('fails', () => {
-    expect(greet('World')).toBe('Hello, world');
-  });
 });
